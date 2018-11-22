a6c6e7d88d318cb8ebfab53a47882d9638b5f881
Josh Brucker <joshbrucker@gmail.com>
2018-11-22 00:49:06 -0500

    Limit emoji search to hub server

--- etc/doki-react.js
+++ etc/doki-react.js
@@ -2,22 +2,23 @@ const utils = require(__basedir + '/utils/utils');
 
 var dokiReact = function(message, client) {
     
+    const dokiHub = client.guilds.find((guild) => guild.id === '413064571075559425');
     var content = message.content.toLowerCase();
 
     if (content.search('monika') > -1) {
-        const monika_head = client.emojis.find((emoji) => emoji.name === 'monika_head');
+        const monika_head = dokiHub.emojis.find((emoji) => emoji.name === 'monika_head');
         message.react(monika_head);
     }
     if (content.search('natsuki') > -1) {
-        const natsuki_head = client.emojis.find((emoji) => emoji.name === 'natsuki_head');
+        const natsuki_head = dokiHub.emojis.find((emoji) => emoji.name === 'natsuki_head');
         message.react(natsuki_head);
     }
     if (content.search('sayori') > -1) {
-        const sayori_head = client.emojis.find((emoji) => emoji.name === 'sayori_head');
+        const sayori_head = dokiHub.emojis.find((emoji) => emoji.name === 'sayori_head');
         message.react(sayori_head);
     }
     if (content.search('yuri') > -1) {
-        const yuri_head = client.emojis.find((emoji) => emoji.name === 'yuri_head');
+        const yuri_head = dokiHub.emojis.find((emoji) => emoji.name === 'yuri_head');
         message.react(yuri_head);
     }
 }
