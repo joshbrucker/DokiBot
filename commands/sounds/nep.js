35dfd1fecdfbc8eef05e5cf5b014a45c9d6b214c
Josh Brucker <joshbrucker@gmail.com>
2019-03-28 22:13:49 -0400

    Fix emoji not appearing in message

--- commands/sounds/nep.js
+++ commands/sounds/nep.js
@@ -15,48 +15,48 @@ let nep = async function(message, args) {
     }
 
     soundUtils.runVoiceCmd(id, channel, async (server, task) => {
-	   	let path = './assets/nep_audio/';
-	    let character;
-	    if (args.length == 0) {
-	        folders = fs.readdirSync(path);
-	        character = folders[Math.floor(Math.random() * folders.length)];
-	    } else {
-	        character = args[0];
-	    }
+        let path = './assets/nep_audio/';
+        let character;
+        if (args.length == 0) {
+            folders = fs.readdirSync(path);
+            character = folders[Math.floor(Math.random() * folders.length)];
+        } else {
+            character = args[0];
+        }
 
-	    path += character;
+        path += character;
 
-	    await new Promise((resolve, reject) => {
-	        fs.readdir(path, async (err, files) => {
-	            if (err) {
-	                channel.send('Can\'t find character ' + character + '!');
-	            } else {
-	                let random = Math.floor(Math.random() * files.length);
-	                path += '/' + files[random];
+        await new Promise((resolve, reject) => {
+            fs.readdir(path, async (err, files) => {
+                if (err) {
+                    channel.send('Can\'t find character ' + character + '!');
+                } else {
+                    let random = Math.floor(Math.random() * files.length);
+                    path += '/' + files[random];
 
-	                let emoji = client.emojis.get('522202578818301970');
+                    let emoji = client.emojis.get('522202578818301970');
 
-	                if (message.guild.voiceConnection) {
-	                    channel.send('Nepu nepu! ' + emoji);
-	                    soundUtils.playSound(message, message.guild.voiceConnection, path);
-	                } else {
-	                    try {
-	                        let conn = await message.member.voice.channel.join();
-	                        channel.send('Nepu nepu! ' + emoji);
-	                        soundUtils.playSound(message, conn, path);
-	                    } catch (err) {
-	                        if (err.toString() == 'Error [VOICE_JOIN_CHANNEL]: You do not have permission to join this voice channel.') {
-	                            channel.send("I don't have access to your voice channel! :frowning:");
-	                        } else {
-	                            throw err;
-	                        }
-	                    }
-	                }
-	            }
+                    if (message.guild.voiceConnection) {
+                        channel.send('Nepu nepu! ' + emoji.toString());
+                        soundUtils.playSound(message, message.guild.voiceConnection, path);
+                    } else {
+                        try {
+                            let conn = await message.member.voice.channel.join();
+                            channel.send('Nepu nepu! ' + emoji.toString());
+                            soundUtils.playSound(message, conn, path);
+                        } catch (err) {
+                            if (err.toString() == 'Error [VOICE_JOIN_CHANNEL]: You do not have permission to join this voice channel.') {
+                                channel.send("I don't have access to your voice channel! :frowning:");
+                            } else {
+                                throw err;
+                            }
+                        }
+                    }
+                }
 
-	            resolve();
-	        });
-	    });
+                resolve();
+            });
+        });
     });
 };
 
