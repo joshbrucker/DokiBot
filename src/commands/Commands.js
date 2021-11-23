57191b88e0b913188befcda669a836b02ea6f55e
BuildTools <unconfigured@null.spigotmc.org>
2021-11-23 12:23:18 -0500

    Change moniquote to be a slash command.

--- src/commands/Commands.js
+++ src/commands/Commands.js
@@ -6,6 +6,7 @@ Commands.set("anime", require("./anilist/anime.js"));
 Commands.set("aniuser", require("./anilist/aniuser.js"));
 Commands.set("character", require("./anilist/character.js"));
 Commands.set("manga", require("./anilist/manga.js"));
+Commands.set("moniquote", require("./fun/moniquote.js"));
 Commands.set("waifu", require("./images/waifu.js"));
 
 // Below is not looked at yet
--- src/commands/fun/moniquote.js
+++ src/commands/fun/moniquote.js
@@ -1,30 +1,47 @@
+const Discord = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
 const fs = require("fs");
 
-let moniquote = async function(client, guild, message, args) {
-  let text = await fs.promises.readFile(__basedir + "/resources/moniquote.txt");
 
-  let lines = text.toString().split('\n');
-  let startIndices = [0];
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("moniquote")
+    .setDescription("Replies with a quote from Monica."),
 
-  for (let i = 0; i < lines.length; i++) {
-    if (lines[i].replace(/[\n\r]/g, "") === "" && lines[i + 1] !== undefined) {
-      startIndices.push(i + 1);
+  async execute(interaction) {
+    let text = await fs.promises.readFile(__basedir + "/resources/moniquote.txt");
+
+    let lines = text.toString().split('\n');
+    let startIndices = [0];
+
+
+    for (let i = 0; i < lines.length; i++) {
+      if (lines[i].replace(/[\n\r]/g, "") === "" && lines[i + 1] !== undefined) {
+        startIndices.push(i + 1);
+      }
     }
+
+    let quoteNum = Math.floor(Math.random() * startIndices.length);
+
+    let msg = "";
+    let title = lines[startIndices[quoteNum]];
+
+    let i = 1;
+    let currLine = lines[startIndices[quoteNum] + i];
+    while (currLine && currLine.replace(/[\r\n]/g, "") !== "") {
+      msg += " " + currLine.replace(/"/g, "").replace(/[\r\n]/g, "\n\n").replace("[player]", interaction.user.username);
+      currLine = lines[startIndices[quoteNum] + ++i];
+    }
+
+    msg = msg.slice(0, msg.length - 1);
+
+    interaction.reply(new Discord.MessagePayload(interaction.channel, {
+      embeds: [
+        new Discord.MessageEmbed({
+          title: title,
+          description: msg
+        })
+      ]
+    }));
   }
-
-  let quoteNum = Math.floor(Math.random() * startIndices.length);
-  let msg = "\`\`\`ml\n" + lines[startIndices[quoteNum]] + "\n\n\"";
-
-  let i = 1;
-  let currLine = lines[startIndices[quoteNum] + i];
-  while (currLine && currLine.replace(/[\r\n]/g, "") !== "") {
-    msg += ' ' + currLine.replace(/["]/g, "").replace(/[\r\n]/g, ' ').replace("[player]", message.member.displayName);
-    currLine = lines[startIndices[quoteNum] + ++i];
-  }
-
-  msg = msg.slice(0, msg.length - 1);
-  msg += "\"\`\`\`";
-  message.channel.send(msg); 
-}
-
-module.exports = moniquote;
+};
