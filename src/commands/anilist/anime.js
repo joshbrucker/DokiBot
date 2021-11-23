b3bb1d00e30a009808314afacb558c161e4fddab
BuildTools <unconfigured@null.spigotmc.org>
2021-11-22 20:54:23 -0500

    Convert aniuser, character, and manga to slash commands.

--- src/commands/anilist/anime.js
+++ src/commands/anilist/anime.js
@@ -3,16 +3,15 @@ const anilist = require(__basedir + "/external_services/anilist/anilist.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('anime')
-    .setDescription('Searches Anilist for an anime.')
+    .setName("anime")
+    .setDescription("Searches Anilist for an anime.")
     .addStringOption(option =>
-      option.setName('title')
-        .setDescription('The title of the anime to search for.')
+      option.setName("title")
+        .setDescription("The title of the anime to search for.")
         .setRequired(true)),
 
   async execute(interaction) {
     let title = interaction.options.get("title").value;
-
     let variables = { search: title };
     let query = `
       query ($search: String) {
@@ -29,10 +28,9 @@ module.exports = {
       let data = json.data.Media;
       interaction.reply("https://anilist.co/anime/" + data.id);
     } else if (res.status === 404) {
-      interaction.reply("I can\'t find that anime!");
+      interaction.reply("I can't find that anime!");
     } else {
-      interaction.reply("I\'m having some issues reaching Anilist. Is it down?");
-      console.log("Anilist request status:" + res.status);
+      interaction.reply("I'm having some issues reaching Anilist. Is it down?");
     }
   },
 };
--- src/commands/anilist/aniuser.js
+++ src/commands/anilist/aniuser.js
@@ -1,29 +1,36 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
 const anilist = require(__basedir + "/external_services/anilist/anilist.js");
 
-let aniuser = async function(client, guild, message, args) {
-  let channel = message.channel;
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("aniuser")
+    .setDescription("Searches Anilist for a user.")
+    .addStringOption(option =>
+      option.setName("name")
+        .setDescription("The name of the user to search for.")
+        .setRequired(true)),
 
-  let name = args.join(' ');
-  let variables = { search: name };
-  let query = `
-    query ($search: String) {
-      User (search: $search) {
-        id
+  async execute(interaction) {
+    let name = interaction.options.get("name").value;
+    let variables = { search: name };
+    let query = `
+      query ($search: String) {
+        User (search: $search) {
+          id
+        }
       }
+    `;
+
+    let res = await anilist.query(query, variables);
+
+    if (res.ok) {
+      let json = await res.json();
+      let data = json.data.User;
+      interaction.reply("https://anilist.co/user/" + data.id);
+    } else if (res.status === 404) {
+      interaction.reply("I can't find that user!");
+    } else {
+      interaction.reply("I'm having some issues reaching Anilist. Is it down?");
     }
-  `;
-
-  let res = await anilist.query(query, variables);
-  if (res.ok) {
-    let json = await res.json();
-    let data = json.data.User;
-    channel.send("https://anilist.co/user/" + data.id);
-  } else if (res.status === 404) {
-    channel.send("I can't find that user!");
-  } else {
-    channel.send("I'm having some issues reaching Anilist. Is it down?");
-    console.log("Anilist request status:" + res.status);
   }
-}
-
-module.exports = aniuser;
+};
