972b70c5941c941e02c8210e839603091a4e25e0
Josh Brucker <joshbrucker@gmail.com>
2022-06-25 13:10:13 -0400

    Update object values when updating values in DB models.

--- src/database/models/GuildModel.js
+++ src/database/models/GuildModel.js
@@ -15,31 +15,37 @@ class GuildModel {
   }
 
   async updateCurrentPoem(poem) {
+    this.currentPoem = poem;
     await runQuery(`UPDATE guild SET current_poem=? WHERE id=?;`, [ poem, this.id ]);
     Cache.del([this.cacheKey]);
   }
 
   async updatePoemChannel(channelId) {
+    this.poemChannel = channelId;
     await runQuery(`UPDATE guild SET poem_channel=? WHERE id=?;`, [ channelId, this.id ]);
     Cache.del([this.cacheKey]);
   }
 
   async updateNextPoemUpdateTime(date) {
+    this.nextPoemUpdateTime = date;
     await runQuery(`UPDATE guild SET next_poem_update_time=? WHERE id=?;`, [ date, this.id ]);
     Cache.del([this.cacheKey]);
   }
 
   async updatePoemFrequency(frequency) {
+    this.poemFrequency = frequency;
     await runQuery(`UPDATE guild SET poem_freq=? WHERE id=?;`, [ frequency, this.id ]);
     Cache.del([this.cacheKey]);
   }
 
   async updateAllowInsults(allow) {
+    this.allowInsults = allow;
     await runQuery(`UPDATE guild SET allow_insults=? WHERE id=?;`, [ allow, this.id ]);
     Cache.del([this.cacheKey]);
   }
 
   async updateNextInsultTime(date) {
+    this.nextInsultTime = date;
     await runQuery(`UPDATE guild SET next_insult_time=? WHERE id=?;`, [ date, this.id ]);
     Cache.del([this.cacheKey]);
   }
