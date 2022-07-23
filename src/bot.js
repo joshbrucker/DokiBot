8dd7559c26125fb85a54f6bac1c4a3292b37fd49
Josh Brucker <joshbrucker@gmail.com>
2022-07-23 01:35:56 -0400

    Resolve error with trying to cache across shards on startup.

--- src/bot.js
+++ src/bot.js
@@ -3,17 +3,17 @@ global.__basedir = __dirname;
 const Discord = require("discord.js");
 
 const auth = require("./auth.json");
-const event_handler = require("./event_handlers/event-handler.js");
+const eventHandler = require("./event_handlers/event-handler.js");
 
 const client = new Discord.Client({ intents: [ "GUILDS", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGES" ] });
 
-client.on("ready", () => event_handler.onClientReady(client));
+client.on("ready", () => eventHandler.onClientReady(client));
 
-client.on("interactionCreate", (interaction) => event_handler.onInteractionCreate(client, interaction));
-client.on("guildCreate", (guild) => event_handler.onGuildCreate(guild));
-client.on("guildDelete", (guild) => event_handler.onGuildDelete(guild));
-client.on("messageCreate", (message) => event_handler.onMessage(client, message));
-client.on("messageReactionAdd", (reaction, user) => event_handler.onMessageReactAdd(client, reaction));
-client.on("messageReactionRemove", (reaction, user) => event_handler.onMessageReactRemove(client, reaction));
+client.on("interactionCreate", (interaction) => eventHandler.onInteractionCreate(client, interaction));
+client.on("guildCreate", (guild) => eventHandler.onGuildCreate(guild));
+client.on("guildDelete", (guild) => eventHandler.onGuildDelete(guild));
+client.on("messageCreate", (message) => eventHandler.onMessage(client, message));
+client.on("messageReactionAdd", (reaction, user) => eventHandler.onMessageReactAdd(client, reaction));
+client.on("messageReactionRemove", (reaction, user) => eventHandler.onMessageReactRemove(client, reaction));
 
 client.login(auth.token);
