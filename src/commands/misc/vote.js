f3f0ed57de608d4fa3106deb60c69b025b975dc1
Josh Brucker <joshbrucker@gmail.com>
2024-04-12 00:20:15 -0400

    Remove unused command logic

--- src/commands/misc/vote.js
+++
@@ -1,6 +0,0 @@
-let vote = function(client, guild, message) {
-  message.channel.send("You can vote for me at the link below! Voting resets any active cooldowns.\n" +
-                       "https://discordbots.org/bot/412824514414510080");
-};
-
-module.exports = vote;
