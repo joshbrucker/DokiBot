cc2b2ad5c2e6fd211c0cbbc57e3cb1f666512cca
Joshua Brucker <joshbrucker@gmail.com>
2026-01-01 14:36:55 -0500

    First pass at basic prometheus metrics

---
+++ src/utils/metrics.js
@@ -0,0 +1,16 @@
+const promClient = require('prom-client');
+
+// Collect default Node.js metrics (CPU, memory, etc.)
+promClient.collectDefaultMetrics();
+
+// Custom metrics
+const commandsCounter = new promClient.Counter({
+  name: 'discord_commands_total',
+  help: 'Total number of Discord commands used',
+  labelNames: ['command', 'shard_id'],
+});
+
+module.exports = {
+  promClient,
+  commandsCounter,
+};
--- src/webserver.js
+++ src/webserver.js
@@ -3,22 +3,14 @@ const Topgg = require("@top-gg/sdk");
 
 const auth = require("./auth.json");
 const settings = require("./settings.json");
+const { promClient } = require("./utils/metrics.js");
 const GlobalMemberAccessor = require("./database/accessors/GlobalMemberAccessor.js");
 
-function startWebserver() {
-  const app = express();
-
-  if (settings.webserver.enableTopggWebhook) {
-    startTopggWebhook(app);
-  }
-}
-
-function startTopggWebhook(app) {
+function startTopggWebhook() {
+  const topggWebhookApp = express();
   const webhook = new Topgg.Webhook(auth.topggWebhookAuth);
 
-  app.post(
-    "/dblwebhook",
-    webhook.listener(async (vote) => {
+  topggWebhookApp.post("/dblwebhook", webhook.listener(async (vote) => {
       // Reset insult submit cooldown
       const globalMemberData = await GlobalMemberAccessor.get(vote.user);
       await globalMemberData.updateNextSubmitDate(new Date());
@@ -26,9 +18,26 @@ function startTopggWebhook(app) {
   );
 
   const port = settings.webserver.topggWebhookPort;
-  app.listen(port, () => {
+  topggWebhookApp.listen(port, () => {
     console.log(`Started Top.gg webhook at /dblwebhook on port ${port}`);
   });
 }
 
-module.exports = startWebserver;
+let prometheusPort = settings.webserver.prometheusWebhookPort;
+
+function startPrometheusMetrics() {
+  const prometheusMetricsApp = express();
+
+  prometheusMetricsApp.get('/metrics', async (req, res) => {
+    res.set('Content-Type', promClient.register.contentType);
+    res.end(await promClient.register.metrics());
+  });
+
+  // For sharding, we need to keep incrementing the port.
+  const port = prometheusPort++;
+  prometheusMetricsApp.listen(port, () => {
+    console.log(`Prometheus metrics running on port ${port}`);
+  });
+}
+
+module.exports = { startTopggWebhook, startPrometheusMetrics };
